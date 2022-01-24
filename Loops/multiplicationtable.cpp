49e10b2195ece29046f401d232c9c4a1aeb64227
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-01-24 22:17:35 +0530

    Multiplication table done

---
+++ Loops/multiplicationtable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"Enter the number you want multiplication table for: "<<endl;
+    cin>>n;
+
+    for(i=1;i<=10;i++)
+    {
+        cout<<n<<"x"<<i<<"="<<i*n<<endl;
+    }
+    return 0;
+}
