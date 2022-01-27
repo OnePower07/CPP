c04c46bdc30103d4141e02b9d323bb89da057f90
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-01-27 17:01:56 +0530

    Question on loops ongoing

---
+++ Loops/perfectnum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n, sum=0;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            {
+                sum=sum+i;
+            } 
+    }
+    {
+        if(2*n==sum)
+        cout<<"Perfect number"<<endl;
+        else
+        cout<<"Not a perfet number";
+    }
+    return 0;
+}
