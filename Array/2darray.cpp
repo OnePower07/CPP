41e26d3eab2bba7a1041fee1e9e307efb1fc0ff9
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-04 00:07:59 +0530

    CPP

---
+++ Array/2darray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A[2][3]={{2,4,6},{3,6,9}};
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++){
+            cout<<A[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
---
+++ Loops/lowercrosstriangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n=4;
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            if(i+j>=n)
+            cout<<"*";
+            else{
+            cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
