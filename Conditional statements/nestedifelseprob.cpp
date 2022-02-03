8d554c5fdcd0cef55333a107b6f2d629d5ccadee
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-03 16:30:44 +0530

    CPP

---
+++ Conditional statements/nestedifelseprob.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"Enter the value of m: "<<endl;
+    cin>>m;
+    if(m>0){
+        cout<<"n=1"<<endl;
+    }
+        else if(m==0){
+            cout<<"n=0"<<endl;
+    }
+    else{
+        cout<<"n=-1"<<endl;
+    }
+    return 0;
+}
