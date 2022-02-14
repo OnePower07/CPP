839915bcb9a294c6703a77d7f2a4837a6745a073
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-14 15:50:41 +0530

    CPP

--- Array/mutiplytwomatrices.cpp
+++ Array/mutiplytwomatrices.cpp
@@ -1,20 +1,59 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int A[2][2]={{2,3,},{3,5}};
-    int B[2][2]={{3,5},{3,5}};
-    int C[2][2];
-
-    for(int i=0; i<2; i++){
-        for(int j=0;j<2;j++){
-            C[2][2]=A[2][2]*B[2][2];
-        }
-    }
-    for(int i=0; i<2 ; i++){
-        for(int j=0; j<2 ;j++){
-            cout<<C[2][2];
-        }
-        cout<<endl;
-    }
-    return 0;
+int main()
+{
+ int a[10][10], b[10][10], mult[10][10], r1, c1, r2, c2, i, j, k;
+ 
+ cout << "Enter rows and columns for first matrix: ";
+ cin >> r1 >> c1;
+ cout << "Enter rows and columns for second matrix: ";
+ cin >> r2 >> c2;
+ 
+ if (c1!=r2)
+ {
+ cout<<"Cant be Multiplied"; 
+ return 0; 
+}
+ 
+ // Storing elements of first matrix.
+ cout << endl << "Enter elements of matrix 1:" << endl;
+ for(i = 0; i < r1; ++i)
+ for(j = 0; j < c1; ++j)
+ {
+ cout << "Enter element a" << i + 1 << j + 1 << " : ";
+ cin >> a[i][j];
+ }
+ 
+ // Storing elements of second matrix.
+ cout << endl << "Enter elements of matrix 2:" << endl;
+ for(i = 0; i < r2; ++i)
+ for(j = 0; j < c2; ++j)
+ {
+ cout << "Enter element b" << i + 1 << j + 1 << " : ";
+ cin >> b[i][j];
+ }
+ 
+ 
+ // Multiplying matrix a and b and storing in array mult.
+ for(i = 0; i < r1; ++i)
+ for(j = 0; j < c2; ++j) 
+ { 
+ mult[i][j]=0;
+ for(k = 0; k < c1; ++k)
+ {
+ mult[i][j] += a[i][k] * b[k][j];
+ } 
+ }
+ 
+ // Displaying the multiplication of two matrix.
+ cout << endl << "Output Matrix: " << endl;
+ for(i = 0; i < r1; ++i)
+ for(j = 0; j < c2; ++j)
+ {
+ cout << " " << mult[i][j];
+ if(j == c2-1)
+ cout << endl;
+ }
+ 
+ return 0;
 }
