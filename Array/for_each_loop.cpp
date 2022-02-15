d2bf5a671cebddb81141ea50e2b3cbcbeb78e929
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-15 23:54:32 +0530

    CPP

--- Array/for_each_loop.cpp
+++ Array/for_each_loop.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int A[4]={7,3,2,1};
+    int A[4]={7,3,3,2},sum=0;
     for(auto i:A)
     {
-        cout<<i<<endl; 
+        cout << A[4] << endl;
     }
+    cout<<"Sum is: "<<sum;
     return 0;
 } 
