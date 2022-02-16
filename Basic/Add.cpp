ad22c3cfbf5231ca37b4aa9a957f6832141f2bd7
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-16 21:52:44 +0530

    CPP

--- Basic/Add.cpp
+++ Basic/Add.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int a, b, c,d;
-    cout << "Enter two Numbers:" << endl;
+    cout << "Enter two Numbers: " << endl;
     cin >> a >> b;
     c = a + b;
     //d = a%b;
