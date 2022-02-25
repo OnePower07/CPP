374d629981f948398c99fe4c398e5750ffd5e224
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-25 16:56:31 +0530

    CPP

--- Pointers/Declaration.cpp
+++ Pointers/Declaration.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int main()
 {
     int a=10;
-    int *p=&a;
-    cout<<a;
+    int *p; //declaration
+    p=&a; //initialisation
+    cout<<a<<endl;
+    cout<<&a<<endl;;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p;
+
 }
