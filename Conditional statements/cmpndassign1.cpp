db2befeb0f2273501515d85215f4049f47c14f4e
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-26 22:07:34 +0530

    CPP

--- Conditional statements/cmpndassign1.cpp
+++ Conditional statements/cmpndassign1.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int fact=100, a=12, b=11, c=1;
     
-    fact*=a*b*c;
+    fact*=a*b*c; //multiplication with the help compound assignment
     
     cout << "Product is: \n"
          << fact;
