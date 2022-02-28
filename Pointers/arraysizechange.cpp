7645eb02910b72a5768acd0301fbab3ad4abda3f
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-28 21:42:11 +0530

    CPP

---
+++ Pointers/arraysizechange.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int *p=new int [20]; //pointer p is pointing on the array of size 20
+    delete []p;
+    p=new int[30];
+    return 0;
+}
