932f5d76a3fe231c887096056b786ba7ec6081c1
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-01 01:28:48 +0530

    CPP

--- Pointers/pointerarithmetic.cpp
+++ Pointers/pointerarithmetic.cpp
@@ -4,8 +4,8 @@ int main()
 {
     int A[5]={2,4,6,8,10};
     int *p = A;
-    //int *q =&A[3];
-    //int d=q-p;
+    int *q =&A[3];
+    int d=q-p;
     //p++;
-    cout<<*p<<endl;
+    cout<<d<<endl;
 }
