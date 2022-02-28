20f331e8c873dc159d00824fb6ac4aae671b73ee
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-02-28 21:43:29 +0530

    CPP

--- Pointers/arraysizechange.cpp
+++ Pointers/arraysizechange.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int *p=new int [20]; //pointer p is pointing on the array of size 20
-    delete []p;
+    delete []p; //above declared array is of no use now.
     p=new int[30];
     return 0;
 }
