06eb474d72515b4d0f590cdda390d298ead0dfc3
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-03 11:28:55 +0530

    CPP

--- Conditional statements/getlinefirstuse.cpp
+++ Conditional statements/getlinefirstuse.cpp
@@ -5,6 +5,7 @@ int main()
     string name;
     cout<<"May i know your name?"<<endl;
     getline(cin,name);//this will take full name
+                    // cin only takes first name
 
     cout<<"Welcome Mr./Mrs. "<<name;
 
