1706df768e445644b1cc9467dfc1e7254704a444
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-05 14:51:05 +0530

    CPP

---
+++ Competetive_programming/Max_MinofArr.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int min,max,i;
+    int A[5]={2,1,6,9,8};
+    min = A[0];
+    max = A[0];
+    for (int i=0;i<5;i++)
+    {
+        if(A[i]<min)
+        {
+            min=A[i];
+        }
+        else if(A[i]>max)
+        {
+            max=A[i];
+        }
+    }
+    cout<<"Max = "<<max<<endl;
+    cout<<"Min = "<<min<<endl;
+}
