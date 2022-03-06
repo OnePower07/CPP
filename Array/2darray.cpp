ff25cff71b0d02385fecd5acbce8613ea7084a43
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-06 21:55:46 +0530

    CPP

--- Array/2darray.cpp
+++ Array/2darray.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int A[2][3]={{2,4,6},{3,6,9}};//2d
+    int A[2][3]={{2,4,6},{3,6,9}};//2d Array
     for(int i=0;i<2;i++)
     {
         for(int j=0;j<3;j++){
