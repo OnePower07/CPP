9697b441419bc6993060f275f86d92ff441509c4
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-10 00:26:26 +0530

    CPP

--- Array/binary_search.cpp
+++ Array/binary_search.cpp
@@ -22,7 +22,6 @@ int main()
         else if (key < A[mid])
         { /*otherwise if the key value is then the mid value then search goes for left side 
                                where mid value decreased by 1 */
-                               
             h = mid - 1;
         }
         else
