866f327d63b48a6d3d22c33de8dd07613746ff37
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-16 16:58:00 +0530

    CPP

--- Conditional statements/cmpndassign1.cpp
+++ Conditional statements/cmpndassign1.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int fact=100, a=12, b=11, c=1;
     
-    fact*=a*b*c; //multiplication with the help compound assignment
+    fact*=a*b*c; //multiplication with the help of compound assignment
     
     cout << "Product is: "<< fact<<endl;;
 
