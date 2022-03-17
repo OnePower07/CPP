26137db3eddd7dcd58e51314bb25d908bf5575d5
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-17 19:29:58 +0530

    CPP

---
+++ Pointers/pointerquest1.cpp
@@ -0,0 +1,11 @@
+//Write a program to print a number which is entered from keyboard using pointer.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter a number: "<<endl;
+    cin>>x;
+    cout<<"The number is: "<<*(&x);
+
+}
