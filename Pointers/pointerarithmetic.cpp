5b4ef78f294be25f77f0a95652579b05b847eca3
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-25 21:25:36 +0530

    CPP

--- Pointers/pointerarithmetic.cpp
+++ Pointers/pointerarithmetic.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-    int A[5]={2,4,6,8,10};
-    int *p = A;
-    //int *q =&A[3];
-    //int d=q-p;
-    //p++;
-    for (int i=0; i<5 ; i++){
-    cout<<i[A]<<endl;
-    }
+    char A[5]={'a','b','c','d','e'};
+    char *p = A;
+    // 200, 204, 208, 212, 216
+    char *q = &A[3];
+    int d = q-p;
+    // p++;
+    cout << d;
+    // for (int i=0; i<5 ; i++){
+    // cout<<i[A]<<endl;
+    // }
 }
