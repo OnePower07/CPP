0ea75cf1cf80482f224918e13ddef4fad4c5438c
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-29 14:23:15 +0530

    CPP

---
+++ Functions/functnoverloading.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int test(int a)
+{}
+float test(double c){} //Overloaded functions may or may not have different return types
+                       // but they must have different arguments
+
+
+
+
+//This will throw an error 
+// int test(int a){}  
+// double test (int b){}
