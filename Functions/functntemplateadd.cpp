53dc15fcd1424ce96f219879846e2ed33d36c962
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-30 22:46:13 +0530

    CPP

--- Functions/functntemplateadd.cpp
+++ Functions/functntemplateadd.cpp
@@ -5,9 +5,18 @@ T add(T x, T y, T z)
 {
     return x+y+z;
 }
+
+// auto add2(auto a, auto b, auto c)
+// {
+//     return a+b+c;
+// }
+
 int main()
 {
     int a=12, b=12, c=22, d;
     cout<<"Sum is: "<<add(a,b,c)<<endl;
+    // auto k = add2('a', 'b', 'c');
+    // cout << k << endl;
+    // cout << typeid(k).name() << endl;
     return 0;
 }
