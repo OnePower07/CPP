90083a724f256d69bec7a4ae3e482cb0d69561ca
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-30 17:26:53 +0530

    CPP

---
+++ Functions/Sumusingfncnoverld.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int sum(int a, int b)
+{
+    return a+b;
+}
+float sum(float a, float b)
+{
+    return a+b;
+}
+int sum(int a, int b, int c)
+{
+    return a,b,c;
+}
+int main()
+{
+  cout<<"Sum is: "<<sum(4,5)<<endl;
+  cout<<"Sum is: "<<sum(4.4f,5.3f)<<endl;
+  cout<<"Sum is: "<<sum(3,5)<<endl;
+}
---
+++ Functions/fncnovrlding1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int absolute(int a,int b)
+{
+if(a<0){
+    a=-a;
+    return a;
+}
+}
+float absolute(float b)
+{
+if(b<0.0){
+    b=-b;
+    return b;
+}
+}
+int main()
+{
+    cout<<"Absolute value is: "<<absolute(-5)<<endl;
+    cout<<"Absolute value is: "<<absolute(-5.443f)<<endl;
+    return 0;
+}
