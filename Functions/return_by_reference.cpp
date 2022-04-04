3a5af7dd763b2ce6d0d9dd3e45b468d98fb0f349
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-04 18:46:31 +0530

    CPP

--- Functions/return_by_reference.cpp
+++ Functions/return_by_reference.cpp
@@ -8,7 +8,7 @@ int & fun(int &a)
 int main()
 {
     int x=10;
-    fun(x)=25; // a is 25  This is the beauty of cpp that u can use the func as L value and can
+    fun(x)=25; // x is 25  This is the beauty of cpp that u can use the func as L value and can
                 // change the value of the variable
     cout<<x<<endl;
 }
