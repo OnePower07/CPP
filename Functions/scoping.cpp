f75d032a52ce1ac0439ba91d194ff5d256cb2ad9
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-06 21:44:03 +0530

    CPP

--- Functions/scoping.cpp
+++ Functions/scoping.cpp
@@ -6,7 +6,7 @@ int main()
     int x=30;
     {
         int x=20;
-        cout<<x<<endl; //x will be the nearest one
+        cout<<x<<endl; //x will take the nearest one
     }
     cout<<x<<endl;
     cout<<::x<<endl;
