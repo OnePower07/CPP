df1ad174591bbec4f9d53b1ea202442768f7d9c2
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-10 00:52:02 +0530

    CPP

---
+++ Functions/Even_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void evenodd(int a)
+{
+     if(a%2==0)
+     {
+         cout<<"a is even"<<endl;
+     }
+     else{
+         cout<<"a is odd"<<endl;
+     }
+}
+int main()
+{
+    int a=11;
+    evenodd(a);
+}
