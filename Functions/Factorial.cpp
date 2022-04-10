e3a415498d05e4ba44d5db4facc602bee5684d64
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-10 20:07:13 +0530

    CPP

---
+++ Functions/Factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+int fact=1;
+if(n==0 || n==1)
+    return fact;
+for(int i=1; i<=n; i++)
+{
+    fact=fact*i;
+}
+return fact;
+}
+int main()
+{
+    int x=5;
+    cout<<factorial(x);
+}
+
+
