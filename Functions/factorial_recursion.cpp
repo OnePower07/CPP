d9dbeb324024cc834b4a4d2e7f17586c0ea83469
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-12 10:58:49 +0530

    CPP

---
+++ Functions/factorial_recursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+    if(n>=1)
+    {
+        return n*factorial(n-1);
+    }
+    else
+    {
+        return 1;
+    }
+}
+int main()
+{
+    int x;
+    cout<<"Enter the number: "<<endl;
+    cin>>x;
+    cout<<"Factorial is: "<<factorial(x);
+    return 0;
+}
