4dd8b4f8a522bc83a76b9c21e04f4472ea635cb9
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-13 20:59:09 +0530

    CPP

--- Functions/functnoverloadingtest.cpp
+++ Functions/functnoverloadingtest.cpp
@@ -5,9 +5,6 @@ int test(int a)
 float test(double c){} //Overloaded functions may or may not have different return types
                        // but they must have different arguments
 
-
-
-
 //This will throw an error 
 // int test(int a){}  
 // double test (int b){}
