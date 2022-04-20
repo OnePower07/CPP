54cb3070cff08a246c293913073d067384c3d730
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-04-20 22:47:04 +0530

    CPP

--- Array/2darray.cpp
+++ Array/2darray.cpp
@@ -4,9 +4,10 @@ int main(){
     int A[2][3]={{2,4,6},{3,6,9}};//2d Array declaration and initialisation
     for(int i=0;i<2;i++)
     {
-        for(int j=0;j<3;j++){
+        for(int j=0;j<3;j++)
+    {
             cout<<A[i][j]<<" ";
-        }
+    }
          cout<<endl;
     }
 
