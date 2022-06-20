460d9e4b96466ac007d798a183ed24fabb7f6d30
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-06-20 23:10:49 +0530

    CPP

---
+++ Inheritance/Intro_to_inheritance.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class base
+{
+    public:
+    int x;
+    void show()
+    {
+        cout<<x<<endl;
+    }
+};
+class derived: public base
+{
+    public:
+    int y;
+    void display()
+    {
+        cout<<x<<" "<<y;
+    }
+};
+int main()
+{
+    base b;
+    b.x=25;
+    b.show();
+
+    derived d;
+    d.x=10;
+    d.y=15;
+    d.show();
+    d.display();
+};
