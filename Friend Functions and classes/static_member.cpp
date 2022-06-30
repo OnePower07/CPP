f52ac5c41e43ab55c094423966ecff030997fa45
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-07-01 00:08:33 +0530

    CPP

---
+++ Friend Functions and classes/static_member.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    public:
+    int a;
+    static int count;
+    test()
+    {
+        a=10;
+        count++;    
+    }
+    static int getcount()
+    {
+        return count;
+    }
+
+};
+int test::count=0;
+int main()
+{
+    test t1,t2;
+    cout<<test::getcount()<<endl;
+    cout<<t1.getcount()<<endl;
+}
